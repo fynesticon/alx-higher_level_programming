bcb216835e582d44e114bdb47802fc2fc14908f6
Ekomobong Cletus <fynesticon@yahoo.com>
2023-04-01 21:26:52 +0100

    Ofline commits

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
+"""Sends a POST request to a given URL with a given email.
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
 """
-Python script that takes in a URL and an email address,
-sends a POST request to the passed URL with the email,
-and displays the body of the response
-"""
-import requests
 import sys
+import requests
 
 
 if __name__ == "__main__":
-    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
     print(r.text)
